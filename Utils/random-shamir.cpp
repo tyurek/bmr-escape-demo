7ed1acdfa098b73eea2cb53dfc9eff19df61648f
Sylvain Bellemare <sbellem@gmail.com>
2022-04-28 01:07:53 -0500

    Update random-shamir.cpp

--- Utils/random-shamir.cpp
+++ Utils/random-shamir.cpp
@@ -1,204 +1,139 @@
 /*
  * Shamir-based random share generator.
  *
+ * Based on paper-example.cpp
+ *
  */
 
 #define NO_MIXED_CIRCUITS
 
-#include "Math/gfp.hpp"
-#include "Protocols/MaliciousRepPrep.hpp"
-#include "Machines/ShamirMachine.hpp"
+// FIXME must appear first
+#include "Machines/SPDZ.hpp"
 
-#include "Tools/ezOptionParser.h"
+#include "Machines/MalRep.hpp"
+#include "Machines/ShamirMachine.hpp"
+#include "Protocols/ProtocolSet.h"
 
 using namespace ez;
 
-int generate(ezOptionParser& opt, int nparties);
+template <class T>
+void run(ez::ezOptionParser &opt, bigint prime, int prime_length);
 
-void Usage(ezOptionParser& opt) {
-	string usage;
-	opt.getUsage(usage);
-	cout << usage;
+void Usage(ez::ezOptionParser &opt) {
+  string usage;
+  opt.getUsage(usage);
+  cout << usage;
 };
 
-int main(int argc, const char** argv)
-{
-    auto& opts = ShamirOptions::singleton;
-    ezOptionParser opt;
-    opts = {opt, argc, argv};
+int main(int argc, const char **argv) {
+  auto &opts = ShamirOptions::singleton;
+  ez::ezOptionParser opt;
+  opts = {opt, argc, argv};
 
-    opt.overview = "Generator of random shares using Shamir Secret Sharing.";
-    opt.syntax = "./random-shamir.x [OPTIONS]\n";
-    opt.example = "./random-shamir.x -i 0 -N 4 -T 1 --nshares 10000 --host darkmatter.io --port 9999 \n";
+  opt.overview = "Generator of random shares using Shamir Secret Sharing.";
+  opt.syntax = "./random-shamir.x [OPTIONS]\n";
+  opt.example = "./random-shamir.x -i 0 -N 4 -T 1 --nshares 10000 --host "
+                "darkmatter.io --port 9999 \n";
 
-    opt.add(
-		"", // Default.
-		0, // Required?
-		0, // Number of args expected.
-		0, // Delimiter if expecting multiple args.
-		"Display usage instructions.", // Help description.
-		"-h",     // Flag token. 
-		"-help",  // Flag token.
-		"--help", // Flag token.
-		"--usage" // Flag token.
-	);
+  opt.add("", 0, 0, 0, "Display usage instructions.", "-h", "--help");
 
-    opt.add(
-		"", // Default.
-		1, // Required?
-		1, // Number of args expected.
-		0, // Delimiter if expecting multiple args.
-		"Player number.", // Help description.
-		"-i", // Flag token.
-		"--playerno" // Flag token.
-	);
+  opt.add("", 1, 1, 0, "Player number.", "-i", "--playerno");
 
-    opt.add(
-		"20000", // Default.
-		0, // Required?
-		1, // Number of args expected.
-		0, // Delimiter if expecting multiple args.
-		"Number of shares to generate (default: 20000).", // Help description.
-		"-s", // Flag token.
-		"--nshares" // Flag token.
-	);
+  opt.add("20000", 0, 1, 0, "Number of shares to generate (default: 20000).",
+          "-s", "--nshares");
 
-    opt.add(
-		"52435875175126190479447740508185965837690552500527637822603658699938581184513", // Default.
-		0, // Required?
-		1, // Number of args expected.
-		0, // Delimiter if expecting multiple args.
-		"Prime field (default: BLS12-381 prime field \
-            '52435875175126190479447740508185965837690552500527637822603658699938581184513').", // Help description.
-		"-P",
-		"--prime"
-	);
+  opt.add("52435875175126190479447740508185965837690552500527637822603658699938"
+          "581184513",
+          0, 1, 0, "Prime field (default: BLS12-381 prime field \
+            '52435875175126190479447740508185965837690552500527637822603658699938581184513').",
+          "-P", "--prime");
 
-    opt.add(
-		"localhost", // Default.
-		0, // Required?
-		1, // Number of args expected.
-		0, // Delimiter if expecting multiple args.
-		"Hostname of MPC server (default: 'localhost').", // Help description.
-		"--host"
-	);
+  opt.add("localhost", 0, 1, 0,
+          "Hostname of MPC server (default: 'localhost').", "--host");
 
-    opt.add(
-		"9999", // Default.
-		0, // Required?
-		1, // Number of args expected.
-		0, // Delimiter if expecting multiple args.
-		"Port number.", // Help description.
-		"-p", // Flag token.
-		"--port" // Flag token.
-	);
+  opt.add("9999", 0, 1, 0, "Port number.", "-p", "--port");
 
-    opt.add(
-          PREP_DIR, // Default.
-          0, // Required?
-          1, // Number of args expected.
-          0, // Delimiter if expecting multiple args.
-          "Preprocessing directory (default: 'Player-Data')", // Help description.
-          "--prep-dir" // Flag token.
-    );
+  opt.add(PREP_DIR, 0, 1, 0, "Preprocessing directory (default: 'Player-Data')",
+          "--prep-dir");
 
-    opt.parse(argc, argv);
+  opt.add("malshamir", 0, 1, 0,
+          "Protocol (shamir or malshamir) (default: 'malshamir')",
+          "--protocol");
 
-	if (opt.isSet("-h")) {
-		Usage(opt);
-		return 1;
-	}
+  opt.parse(argc, argv);
 
-    if (!opt.isSet("-i"))
-    {
-		Usage(opt);
-        exit(0);
-    }
-    cout << "opts threshold: " << opts.threshold << endl;
-    cout << "opts nparties: " << opts.nparties << endl;
-    return generate(opt, opts.nparties);
+  if (opt.isSet("-h")) {
+    Usage(opt);
+    return 1;
+  }
+
+  if (!opt.isSet("-i")) {
+    Usage(opt);
+    exit(0);
+  }
+
+  string prime, protocol;
+  opt.get("--prime")->getString(prime);
+  opt.get("--protocol")->getString(protocol);
+
+  // TODO compute prime length from given prime
+  const int prime_length = 256;
+  const int n_limbs = (prime_length + 63) / 64;
+
+  if (protocol == "shamir")
+    run<ShamirShare<gfp_<0, n_limbs>>>(opt, bigint(prime), prime_length);
+  else if (protocol == "malshamir")
+    run<MaliciousShamirShare<gfp_<0, n_limbs>>>(opt, bigint(prime),
+                                                prime_length);
+  else {
+    cerr << "Unsupported protocol: " << protocol << endl;
+    cerr << "Use shamir or malshamir" << endl;
+    exit(1);
+  }
 }
 
+template <class T>
+void run(ez::ezOptionParser &opt, bigint prime, int prime_length) {
+  int playerno, nparties, nshares, port;
+  string hostname, prep_dir;
+  opt.get("--playerno")->getInt(playerno);
+  opt.get("--nparties")->getInt(nparties);
+  opt.get("--nshares")->getInt(nshares);
+  opt.get("--host")->getString(hostname);
+  opt.get("--port")->getInt(port);
+  opt.get("--prep-dir")->getString(prep_dir);
 
-int generate(ezOptionParser& opt, int nparties)
-{
-    // needed because of bug in some gcc versions < 9
-    // https://gitter.im/MP-SPDZ/community?at=5fcadf535be1fb21c5fce581
-    //bigint::init_thread();
+  Names names(playerno, nparties, hostname, port);
+  CryptoPlayer player(names);
 
-    // bit length of prime
-    const int prime_length = 256;
+  cout << "prime length: " << prime_length << endl;
+  ProtocolSetup<T> setup(prime, player);
 
-    // compute number of 64-bit words needed
-    const int n_limbs = (prime_length + 63) / 64;
-    typedef MaliciousShamirShare<gfp_<0, n_limbs>> T;
+  ProtocolSet<T> set(player, setup);
 
-    //int playerno, nparties, nshares, port;
-    int playerno, nshares, port;
-    string hostname, prime, prep_dir;
-    opt.get("--playerno")->getInt(playerno);
-    //opt.get("--nparties")->getInt(nparties);
-    opt.get("--nshares")->getInt(nshares);
-    opt.get("--prime")->getString(prime);
-    opt.get("--host")->getString(hostname);
-    opt.get("--port")->getInt(port);
-    opt.get("--prep-dir")->getString(prep_dir);
+  auto &preprocessing = set.preprocessing;
 
-    Names N;
-    Server::start_networking(N, playerno, nparties, hostname, port);
-    CryptoPlayer P(N);
+  stringstream ss;
+  ofstream outputFile;
+  string prep_data_dir = get_prep_sub_dir<T>(prep_dir, player.num_players());
+  ss << prep_data_dir << "Randoms-" << T::type_short() << "-P"
+     << player.my_num();
+  outputFile.open(ss.str().c_str());
 
-    // initialize field
-    //gfp_::init_field(bigint(prime));
-    T::clear::init_field(bigint(prime));
-    // TODO: not sure if the BLS prime should also be set on the `next`
-    //T::clear::next::init_default(prime_length, false);
-    T::clear::next::init_field(bigint(prime), false);
+  int ntriples = nshares / 2 + nshares % 2;
+  vector<T> Sa(ntriples), Sb(ntriples), Sc(ntriples);
+  for (int i = 0; i < ntriples; i++) {
+    preprocessing.get_three(DATA_TRIPLE, Sa[i], Sb[i], Sc[i]);
+    Sa[i].output(outputFile, true);
+    if (i == ntriples - 1 && nshares % 2)
+      break;
+    outputFile << "\n";
+    Sb[i].output(outputFile, true);
+    if (i != ntriples - 1)
+      outputFile << "\n";
+  }
 
-    // must initialize MAC key for security of some protocols
-    typename T::mac_key_type mac_key;
-    T::read_or_generate_mac_key("", P, mac_key);
+  cout << "\nDONE!" << endl;
 
-    // TODO check if really needed
-    // global OT setup
-    BaseMachine machine;
-    if (T::needs_ot)
-        machine.ot_setups.push_back({P});
-
-    // TODO check if really needed
-    // keeps tracks of preprocessing usage
-    DataPositions usage;
-    usage.set_num_players(P.num_players());
-
-    // output protocol
-    typename T::MAC_Check output(mac_key);
-
-    // preprocessing
-    typename T::LivePrep preprocessing(0, usage);
-    SubProcessor<T> processor(output, preprocessing, P);
-
-    stringstream ss;
-    ofstream outputFile;
-    string prep_data_dir = get_prep_sub_dir<T>(prep_dir, P.num_players());
-    ss << prep_data_dir << "Randoms-" << T::type_short() << "-P" << P.my_num();
-    outputFile.open(ss.str().c_str());
-
-    int ntriples = nshares / 2 + nshares % 2;
-    vector<T> Sa(ntriples), Sb(ntriples), Sc(ntriples);
-    for (int i=0; i < ntriples; i++)
-    {
-        preprocessing.get_three(DATA_TRIPLE, Sa[i], Sb[i], Sc[i]);
-        Sa[i].output(outputFile, true);
-        if (i == ntriples - 1 &&  nshares % 2)
-            break;
-        outputFile << "\n";
-        Sb[i].output(outputFile, true);
-        if (i != ntriples - 1)
-            outputFile << "\n";
-    }
-
-    cout << "\nDONE!" << endl;
-
-    T::LivePrep::teardown();
-    return 0;
+  T::LivePrep::teardown();
 }
