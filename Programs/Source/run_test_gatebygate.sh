251637e805f8dbabb7cbeb6c241f4245df8bd966
Tom Yurek <tomyurek@gmail.com>
2023-02-03 20:02:23 -0500

    update gatebygate

--- Programs/Source/run_test_gatebygate.sh
+++ Programs/Source/run_test_gatebygate.sh
@@ -6,19 +6,17 @@ players=4
 threshold=1
 
 # Compile the programs
-for i in test_gatebygate_mult64; do
+for i in test_gatebygate; do
     ./compile.py $i || exit 1
 done
 
 # Setup the network authentication
-Scripts/setup-ssl.sh 4
+#Scripts/setup-ssl.sh 4
 
 # Set up the player inputs
 mkdir Player-Data
-echo 14 > Player-Data/Input-P0-0
-echo 12 > Player-Data/Input-P1-0
-echo 8 > Player-Data/Input-P2-0
-echo 0 > Player-Data/Input-P3-0
+echo "5 -40" > Player-Data/Input-P0-0
+echo "100 100 500 1000" > Player-Data/Input-P1-0
 
 
 progs="./malicious-shamir-party.x"
@@ -26,7 +24,7 @@ progs="./malicious-shamir-party.x"
 for prog in $progs; do
     echo "Combined Offline/Online Experiment $prog"
     for i in 0 1 2 3; do
-	$prog -N 4 -T 1 -p $i -P $BLS_PRIME test_gatebygate_mult64  & pids[${i}]=$!
+	$prog -N 4 -T 1 -p $i -P $BLS_PRIME test_gatebygate  & pids[${i}]=$!
     done
     for pid in ${pids[*]}; do
 	wait $pid
